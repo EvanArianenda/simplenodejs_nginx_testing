9d2edf8d3b8c9db1dd78611d9fbf23e433e37675
EvanArianenda <155868564+EvanArianenda@users.noreply.github.com>
2024-01-07 15:48:18 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
-const http = require("http");
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end("Successfully started a server");
-});
-
-server.listen(4000, "localhost", () => {
-  console.log("Listening for request");
-});
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
